a3df204a22bb6ad5232cfd8f4af5052bb35ab17a
rlaehgns5399 <rlaehgns5398@gmail.com>
2018-11-05 23:48:43 +0900

    Add library npgsql, implement connecing to local database

--- XDOErrorDetector/XDOErrorDetector/Program.cs
+++ XDOErrorDetector/XDOErrorDetector/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Npgsql;
 
 namespace XDOErrorDetector
 {
@@ -10,7 +11,40 @@ namespace XDOErrorDetector
     {
         static void Main(string[] args)
         {
-            
+            var info = new DB();
+            using (var conn = new NpgsqlConnection("Host=" + info.Host + ";Username=" + info.Username + ";Password=" + info.Password + ";Database=" + info.Database))
+            {
+                try
+                {
+                    conn.Open();
+                    using(var cmd = new NpgsqlCommand())
+                    {
+                        cmd.Connection = conn;
+                        cmd.CommandText = "select * from xdo";
+
+                        using(var reader = cmd.ExecuteReader())
+                        {
+                            Console.WriteLine("table column 수 = {0} 개", reader.FieldCount);
+
+                            
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+
+            Console.ReadLine();
         }
+    }
+
+    class DB
+    {
+        public String Host = "localhost";
+        public String Username = "postgres";
+        public String Password = "root";
+        public String Database = "mydata";
     }
 }
