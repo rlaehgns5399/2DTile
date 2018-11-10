817118bc9ce590f6813c9c465e906980534ac292
rlaehgns5399 <rlaehgns5398@gmail.com>
2018-11-10 21:49:22 +0900

    Add component for UI work

---
+++ XDOErrorDetectorUI/XDOErrorDetectorUI/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XDOErrorDetectorUI
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            label.Content = "우히히";
+        }
+
+        private void button_update_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void button_check_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
