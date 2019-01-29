8f1e0ee7bc167ba7e85716e7c2c9e6e4b8436635
rlaehgns5399 <rlaehgns5398@gmail.com>
2019-01-29 13:29:44 +0900

    Modify: readxdo, use more elegant way than before

--- XDOErrorDetectorUI/XDOErrorDetectorUI/ReadXDO.cs
+++ XDOErrorDetectorUI/XDOErrorDetectorUI/ReadXDO.cs
@@ -127,11 +127,13 @@ namespace XDOErrorDetectorUI
     {
         public List<Vector3> list_vertex = new List<Vector3>();
         public List<Vector3> list_normal = new List<Vector3>();
+        public List<Vector3> list_normal_modifed = new List<Vector3>();
         public List<Vector2> list_texture = new List<Vector2>();
         public List<ushort> list_indice = new List<ushort>();
 
         public float vertex_minX, vertex_minY, vertex_minZ, vertex_maxX, vertex_maxY, vertex_maxZ;
-
+        public float normal_minX, normal_minY, normal_minZ, normal_maxX, normal_maxY, normal_maxZ;
+        public float texture_minU, texture_minV, texture_maxU, texture_maxV;
         public uint vertexCount;
         public uint indexedCount;
         Color32 Color;
@@ -147,7 +149,24 @@ namespace XDOErrorDetectorUI
             for (int i = 0; i < vertexCount; i++)
             {
                 list_vertex.Add(new Vector3(br.ReadSingle(), br.ReadSingle(), br.ReadSingle()));
-                list_normal.Add(new Vector3(br.ReadSingle(), br.ReadSingle(), br.ReadSingle()));
+
+                var nx = br.ReadSingle();
+                var ny = br.ReadSingle();
+                var nz = br.ReadSingle();
+
+                list_normal.Add(new Vector3(nx, ny, nz));
+
+                if (nx == 0.0f && ny == 0.0f && nz == 0.0f)
+                    nx = ny = nz = 0.0f;
+
+                var n = Math.Abs((float)Math.Sqrt(nx * nx + ny * ny + nz * nz));
+
+                nx /= n;
+                ny /= n;
+                nz /= n;
+
+                list_normal_modifed.Add(new Vector3(nx, ny, nz));
+
                 list_texture.Add(new Vector2(br.ReadSingle(), br.ReadSingle()));
             }
             // getting Min&Max XYZ | UV
@@ -157,6 +176,19 @@ namespace XDOErrorDetectorUI
             vertex_maxY = list_vertex.Select(x => x[1]).Max();
             vertex_minZ = list_vertex.Select(x => x[2]).Min();
             vertex_maxZ = list_vertex.Select(x => x[2]).Max();
+
+            normal_minX = list_normal_modifed.Select(x => x[0]).Min();
+            normal_maxX = list_normal_modifed.Select(x => x[0]).Max();
+            normal_minY = list_normal_modifed.Select(x => x[1]).Min();
+            normal_maxY = list_normal_modifed.Select(x => x[1]).Max();
+            normal_minZ = list_normal_modifed.Select(x => x[2]).Min();
+            normal_maxZ = list_normal_modifed.Select(x => x[2]).Max();
+
+            texture_minU = list_texture.Select(x => x[0]).Min();
+            texture_maxU = list_texture.Select(x => x[0]).Max();
+            texture_minV = list_texture.Select(x => x[1]).Min();
+            texture_maxV = list_texture.Select(x => x[1]).Max();
+
             // indexedCount
             this.indexedCount = br.ReadUInt32();
             for (int i = 0; i < indexedCount; i++)
