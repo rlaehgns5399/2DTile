eb625e358dc09ef76794dcb9ff62974a256c660f
rlaehgns5399 <rlaehgns5398@gmail.com>
2019-02-01 16:25:41 +0900

    Add: slicingXDO project
    implementing basic window

---
+++ SlicingXDO/SlicingXDO/Game.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+
+namespace SlicingXDO
+{
+    class Game
+    {
+        public GameWindow window;
+        public Game(GameWindow windowInput)
+        {
+            this.window = windowInput;
+
+            window.Load += Window_Load;
+            window.RenderFrame += Window_RenderFrame;
+            window.UpdateFrame += Window_UpdateFrame;
+            window.Closing += Window_Closing;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+           
+        }
+
+        private void Window_UpdateFrame(object sender, FrameEventArgs e)
+        {
+
+        }
+
+        private void Window_RenderFrame(object sender, FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Flush();
+            window.SwapBuffers();
+        }
+
+        private void Window_Load(object sender, EventArgs e)
+        {
+            GL.ClearColor(Color.FromArgb(5, 5, 25));
+        }
+    }
+}
---
+++ SlicingXDO/SlicingXDO/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+namespace SlicingXDO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var window = new GameWindow(800, 600);
+            var game = new Game(window);
+
+            window.Run();
+        }
+    }
+}
